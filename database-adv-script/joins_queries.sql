4cd9d2b6867dae76be49945a2f57f9835978a234
aniebietafia <aniebietafia87@gmail.com>
2025-09-03 20:26:34 +0100

    Monitor and Refine Database Performance
    Signed-off-by: aniebietafia <aniebietafia87@gmail.com>

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,5 +1,6 @@
 -- Write a query using anINNER JOIN to retrieve all bookings and the respective users who made those bookings.
-SELECT b.booking_id,
+SELECT 
+    b.booking_id,
     b.start_date,
     b.end_date,
     b.total_price,
@@ -9,7 +10,7 @@ SELECT b.booking_id,
     p.phone_number
 FROM
     bookings AS b
-INNER JOIN 
+INNER JOIN
     profile AS p ON b.user_id = p.user_id;
 
 -- Write a query using aLEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
@@ -18,10 +19,9 @@ SELECT p.property_id,
     p.description,
     r.rating,
     r.comment
-FROM
-    properties AS p
-LEFT JOIN
-    reviews AS r ON p.property_id = r.property_id;
+FROM properties AS p
+    LEFT JOIN reviews AS r ON p.property_id = r.property_id
+ORDER BY p.property_id;
 
 -- Write a query using a FULL OUTER JOIN to retrieve all users and all bookings,
 -- even if the user has no booking or a booking is not linked to a user.
@@ -32,8 +32,5 @@ SELECT p.user_id,
     b.start_date,
     b.end_date,
     b.status
-FROM
-    profile AS p
-FULL OUTER JOIN
-    bookings AS b ON p.user_id = b.user_id;
-
+FROM profile AS p
+    FULL OUTER JOIN bookings AS b ON p.user_id = b.user_id;
